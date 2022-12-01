2fa30b6a7690e3caeee5366e0fdebe344d905cbe
danielconradie12 <118206323+danielconradie12@users.noreply.github.com>
2022-12-01 13:09:49 -0700

    Add security to swagger

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules;
